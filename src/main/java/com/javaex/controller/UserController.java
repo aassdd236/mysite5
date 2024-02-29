2f03753bf239f4296fe62ef6e542b90219115a45
aassdd236 <aassdd236@naver.com>
2024-02-29 16:37:40 +0900

    02/29

---
+++ src/main/java/com/javaex/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping(value="/user/loginform", method= {RequestMethod.GET, RequestMethod.POST})
+	public String loginForm() {
+		System.out.println("UserController.loginForm()");
+		
+		return "user/loginForm";
+	}
+}
